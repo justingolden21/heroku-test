e231dc654019d0d1186c619e667befd51cb32aba
RGB Studios <justingolden21@gmail.com>
2019-09-24 23:28:28 -0700

    add link of todo

--- scripts/increment.php
+++ scripts/increment.php
@@ -1,6 +1,10 @@
 <?php
+
 $current = file_get_contents('../number.json');
 $data = json_decode($current) + 1;
 
 $jsonData = json_encode($data, JSON_PRETTY_PRINT);
 file_put_contents('../number.json', $jsonData);
+
+// TODO:
+// https://github.com/mongolab/mongodb-driver-examples/blob/master/php/php_simple_example.php
